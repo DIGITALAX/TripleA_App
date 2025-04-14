54bdcd3b6d38f615ded28c08aa77f5b38ac8b9fc
emmajane1313 <emma.jane@digitalax.xyz>
2025-04-14 01:16:01 -0400

    graph

--- src/lib/graph/client.ts
+++ src/lib/graph/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const aaaLink = new HttpLink({
-  uri: `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/subgraphs/id/AudwYcFk14weD3LBKdn3kcgvT2JQKas31ZrKrTk8UF3K`,
+  uri: `https://gateway-arbitrum.network.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/subgraphs/id/5XK1Z5BL6TGMmpJV4irttCu4RgAePp7sPLKnPZfXVCcK`,
 });
 
 export const aaaClient = new ApolloClient({
